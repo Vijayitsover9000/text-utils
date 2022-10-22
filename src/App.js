207350ea99bfb5efc58d6b10baf668b3d44aa6f0
Bijay kuamr sah <vijaykumarsah293@gmail.com>
2022-10-22 20:06:32 +0530

    without css text editor

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+
+import Navbar from './Components/Navbar.js';
+import TextForm from './Components/TextForm.js';
+//function App() it can also be written as const App()=>{}
+
+function App() {
+  const[mode,setMode]=useState("light");
+  const toggleMode=()=>{
+    if(mode==="light"){
+      setMode("dark");
+      document.body.style.backgroundColor="black";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor="white";
+    }
+  }
+  return (
+    <>
+      <div>
+          <Navbar mode={mode} toggleMode={toggleMode}/>
+          <TextForm Heading="Enter the text you want to Analyze"/>
+      </div>
+    </>
+  );
+}
+export default App;
